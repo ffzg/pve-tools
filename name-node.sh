c5aa499ed8dd70d3ccbc6ccecc6c21fe0f6c0380
Dobrica Pavlinusic <dpavlin@rot13.org>
2024-07-18 14:10:11 +0200

    added status and type

--- name-node.sh
+++ name-node.sh
@@ -1,1 +1,4 @@
-pvesh get /cluster/resources --type vm --output-format json | tee /dev/shm/resources.json | jq -r '.[] | "\(.vmid) \(.name) \(.node)"'
+#!/bin/sh -e
+
+pvesh get /cluster/resources --type vm --output-format json | tee /dev/shm/resources.json \
+| jq -r '.[] | "\(.vmid) \(.name) \(.status) \(.type) \(.node)"' | column -t
