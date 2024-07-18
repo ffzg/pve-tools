88863a75c0f1f6c89cb3f581c196b2986c2ecac7
Dobrica Pavlinusic <dpavlin@rot13.org>
2024-07-18 15:18:34 +0200

    cpu, mem, disk and header

--- name-node.sh
+++ name-node.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -e
 
+(
+echo VEID NAME STATUS TYPE CPU MEM DISK NODE
 pvesh get /cluster/resources --type vm --output-format json | tee /dev/shm/resources.json \
-| jq -r '.[] | "\(.vmid) \(.name) \(.status) \(.type) \(.node)"' | column -t
+| jq -r '.[] | "\(.vmid) \(.name) \(.status) \(.type) \(.maxcpu) \(.maxmem) \(.maxdisk) \(.node)"' \
+| numfmt --field 6,7 --to iec \
+) | column -t
